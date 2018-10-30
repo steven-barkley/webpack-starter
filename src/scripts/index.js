bb244821b94429ec697d487e925923dfd81b5e9f
Steven Barkley <steven.barkley@outlook.com>
2018-10-30 00:29:36 -0400

    Added more text

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,38 +1,63 @@
-function sendCars(...allCarIds)
-{
-    allCarIds.forEach(id => console.log(id));
-}
+// function sendCars(...allCarIds)
+// {
+//     allCarIds.forEach(id => console.log(id));
+// }
 
-sendCars(100,200,555);
+// sendCars(100,200,555);
 
-function sendCats(day,...allCatIds)
-{
-    allCatIds.forEach(id => console.log(id));
-}
+// function sendCats(day,...allCatIds)
+// {
+//     allCatIds.forEach(id => console.log(id));
+// }
 
-sendCats("Sunday","Tom","Garfield","Lux");
+// sendCats("Sunday","Tom","Garfield","Lux");
 
-let dogIds = [100,200,301];
-let [dog2,...theRest] = dogIds;
+// let dogIds = [100,200,301];
+// let [dog2,...theRest] = dogIds;
 
-console.log(dog2);
+// console.log(dog2);
 
-console.log(dogIds[0],dogIds[1], theRest);
-
-
-let boy = { id: 5000, style: 'convertible'};
-
-let id, style;
-
-({id, style} = boy);
-
-console.log(id,style);
+// console.log(dogIds[0],dogIds[1], theRest);
 
 
-function startCars(car1,car2,car3,...rest){
-    console.log(rest);
-}
+// let boy = { id: 5000, style: 'convertible'};
 
-let carIds = [1,2,3,4,5,6];
-startCars(...carIds);
+// let id, style;
 
+// ({id, style} = boy);
+
+// console.log(id,style);
+
+
+// function startCars(car1,car2,car3,...rest){
+//     console.log(rest);
+// }
+
+// let carIds = [1,2,3,4,5,6];
+// startCars(...carIds);
+
+//Equality Operators
+
+let var1=0;
+let var2='0';
+
+
+if(var1 == var2) 
+    {
+        console.log('Same Value');
+    }
+
+if(var1 === var2)
+    {
+        console.log("Same type and value");
+    }
+
+if(var1 != var2)
+    {
+        console.log("Not equal");
+    }
+
+if (var1 !== var2)
+    {
+        console.log("Not equal or Not same data type");
+    }
